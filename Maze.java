23732f9f4a009e2cb0b1dd5d62fc2bfd11cf7b32
Jack Lam <jlam00@stuy.edu>
2019-02-28 11:43:35 -0500

    completed constructer, add, remove, and toString

--- Maze.java
+++ Maze.java
@@ -1,29 +1,59 @@
 import java.util.*;
 import java.io.*;
 public class Maze{
-  private char[][]maze;
+  private char[][]board;
   private boolean animate;//false by default
+  private int startRow;
+  private int startCol;
+  private int endRow;
+  private int endCol;
 
-public Maze(String filename) throws FileNotFoundException{
+public Maze(String filename){
   animate = false;
   try{
-  File text = new File("Maze.txt");
+  File text = new File(filename);
   Scanner maze = new Scanner(text);
   int row = 0;
   int col = 0;
   while (maze.hasNextLine()){
     String line = maze.nextLine();
-    System.out.println(line);
     col = 0;
     row += 1;
     col = line.length();
     //System.out.println("row: " + row);
     //System.out.println("col: " + col);
   }
-  //maze = new char[row][col];
+  board = new char[row][col];
+  maze = new Scanner(text);
+  row = 0;
+  while(maze.hasNextLine()){
+    String line = maze.nextLine();
+    for (int c = 0; c <line.length(); c ++){
+      if (line.charAt(c) == 'S'){
+        startRow = row;
+        startCol = c;
+      }
+      if (line.charAt(c) == 'E'){
+        endRow = row;
+        endCol = c;
+      }
+      board[row][c] = line.charAt(c);
+    }
+    row +=1;
+  }
 }
 catch(Exception e){}
 }
+public String toString(){
+  String ans = "";
+    for(int r = 0; r < board.length; r ++){
+      for (int c = 0; c < board[r].length; c++){
+        ans += board[r][c];
+      }
+      ans += "\n";
+    }
+    return ans;
+   }
 private void wait(int millis){
      try {
          Thread.sleep(millis);
@@ -40,13 +70,52 @@ public void clearTerminal(){ //erase terminal, go to top left of screen.
 //public int solve(){
         //return solve(???,???)
 //}
+public int solve(){
+board[startRow][startCol] = ' ';
+return solve(startRow, startCol);
+}
+
 private int solve(int row, int col){ //you can add more parameters since this is private
     if(animate){
         clearTerminal();
         System.out.println(this);
         wait(20);
     }
+
     return -1; //so it compiles
+}
+private boolean move(int row, int col, String direction){
+  if(direction.equals("up") && board[row -1][col] == ' '){
+    board[row][col] = '@';
+    return true;
+  }
+  if(direction.equals("down") && board[row +1][col] == ' '){
+    board[row][col] = '@';
+    return true;
+  }
+  if(direction.equals("left") && board[row][col -1] == ' '){
+    board[row][col] = '@';
+    return true;
+  }
+  if(direction.equals("right") && board[row][col + 1] == ' '){
+    board[row][col] = '@';
+    return true;
+  }
+  return false;
+}
+
+private boolean remove(int row, int col){
+  if (board[row][col] == '#'){
+  board[row][col] = '.';
+  return true;
+}
+return false;
+}
+public static void main(String[] args) {
+  Maze maze = new Maze("Maze.txt");
+  maze.clearTerminal();
+  //maze.solve();
+  System.out.println(maze);
 }
 
 
